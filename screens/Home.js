dc608619cfc4f9b73e56b776803540831765f093
strogan <asdof01@gmail.com>
2021-11-11 23:53:17 +0200

    assets

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Categories from "../components/Categories";
 
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
@@ -12,6 +13,7 @@ export default function Home() {
         <HeaderTabs />
         <SearchBar />
       </View>
+      <Categories />
     </SafeAreaView>
   );
 }
